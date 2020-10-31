d2d7e09cac0f8ded7429ab1479684fbdf1456188
mezeia77 <mezeia77@gmail.com>
2020-10-31 11:59:12 +0100

    classstructureio

--- src/main/java/classstructureio/calculator.java
+++ src/main/java/classstructureio/calculator.java
@@ -1,4 +1,19 @@
 package classstructureio;
 
+import java.util.Scanner;
+
 public class calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Number 1?");
+        int number1 = scanner.nextInt();
+
+        System.out.println("Number 2?");
+        int number2 = scanner.nextInt();
+
+        System.out.println(number1 + " + " + number2);
+        System.out.println(number1 + number2);
+
+    }
 }
