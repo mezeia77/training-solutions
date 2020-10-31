b89c2281fdacb6c9ab7d7d6a74cbcd13457c8b94
mezeia77 <mezeia77@gmail.com>
2020-10-31 17:50:27 +0100

    classstructureattributes

---
+++ src/main/java/classsctructureconstructors/Store.java
@@ -0,0 +1,14 @@
+package classsctructureconstructors;
+
+public class Store {
+    private String product;
+    private int stock;
+
+public Store (String product) {this.product = product; stock = 0;}
+public String getProduct() {return product;}
+
+public int getStock() {return stock;}
+
+    public void in (int amount) {stock = stock + amount;}
+    public void out (int amount) {stock = stock - amount;}
+}
