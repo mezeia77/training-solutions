52fa7b9b5e40ba0759db980d67ba7b85b2281a27
mezeia77 <mezeia77@gmail.com>
2020-10-31 18:37:16 +0100

    classstructureattributes

--- src/main/java/classstructureio/calculator.java
+++ src/main/java/classstructureio/calculator.java
@@ -12,7 +12,7 @@ public class calculator {
         System.out.println("Number 2?");
         int number2 = scanner.nextInt();
 
-        System.out.println(number1 + " + " + number2 + " = " + number1 + number2);
+        System.out.println(number1 + " + " + number2 + " = " + (number1 + number2));
 
     }
 }
--- src/main/java/classstructureio/registration.java
+++ src/main/java/classstructureio/registration.java
@@ -1,4 +1,18 @@
 package classstructureio;
 
+import java.util.Scanner;
+
 public class registration {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Name?");
+        String name = scanner.nextLine();
+
+        System.out.println("Email?");
+        String email = scanner.nextLine();
+
+        System.out.println("Registration name: " + name);
+        System.out.println("Registration email: " + email);
+    }
 }
