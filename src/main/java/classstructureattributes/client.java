6b65c132ff6948a9b456b661f841dfcd80340b54
mezeia77 <mezeia77@gmail.com>
2020-10-31 12:40:47 +0100

    classstructureattributes

---
+++ src/main/java/classstructureattributes/client.java
@@ -0,0 +1,11 @@
+package classstructureattributes;
+
+import java.util.Scanner;
+
+public class client {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println();
+    }
+}
