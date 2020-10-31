a542acde7cde937271874105e4775aaabf86e6f6
mezeia77 <mezeia77@gmail.com>
2020-10-31 17:19:51 +0100

    classstructureattributes

---
+++ src/main/java/classsctructureconstructors/BookMain.java
@@ -0,0 +1,14 @@
+package classsctructureconstructors;
+
+public class BookMain {
+    public static void main(String[] args) {
+        Book book = new Book("Novothny ", " Villamos energia rendszerek");
+
+        book.register(99);
+
+        System.out.println("Szerző?" + book.getAuthor());
+        System.out.println("Cím?" + book.getTitle());
+        System.out.println("Reg.szám: " + book.getRegNumber());
+
+    }
+}
