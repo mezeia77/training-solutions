536fdd0ea5f3760b4df8505fbb9a5aaf17ed64f7
mezeia77 <mezeia77@gmail.com>
2020-11-07 15:00:11 +0100

    javaapi

---
+++ src/main/java/javaapi/Upper.java
@@ -0,0 +1,10 @@
+package javaapi;
+
+
+public class Upper {
+    public static void main(String[] args) {
+
+        String message = "hello world!";
+        System.out.println(message.toUpperCase());
+    }
+}
