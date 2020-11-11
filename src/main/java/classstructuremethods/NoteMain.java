0b5af6f629ec19849336e145e6dd004a5097a46e
mezeia77 <mezeia77@gmail.com>
2020-11-11 19:02:38 +0100

    Junit added

--- src/main/java/classstructuremethods/NoteMain.java
+++ src/main/java/classstructuremethods/NoteMain.java
@@ -10,12 +10,12 @@ public class NoteMain {
         System.out.println("Kérem a nevét");
         String name = scanner.nextLine();
         System.out.println(name);
-        note.setName("Kiss Pista");
-        System.out.println("Az Ön valódi neve: " + name);
 
+        note.setName("Kiss Pista");
         note.setTopic("Életrajz");
         note.setText("Az egész úgy kezdődött, hogy...");
 
+        System.out.println("Az Ön valódi neve: " + name);
         System.out.println(note.getNoteText());
 
 
--- src/main/java/stringtype/Registration.java
+++ src/main/java/stringtype/Registration.java
@@ -3,7 +3,7 @@ package stringtype;
 import java.util.Scanner;
 
 public class Registration {
-    public static void main(String[] args) {
+    public static void main (String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Név:");
