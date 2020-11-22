25d90f74097b24fd3b27f7b1ab68aa6429dacf54
mezeia77 <mezeia77@gmail.com>
2020-11-22 12:13:37 +0100

    operators

---
+++ src/main/java/operators/OperatorsMain.java
@@ -0,0 +1,11 @@
+package operators;
+
+public class OperatorsMain {
+    public static void main(String[] args) {
+
+        Operators operators = new Operators();
+
+        System.out.println(operators.isEvan(9));
+
+    }
+}
