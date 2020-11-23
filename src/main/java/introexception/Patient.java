8c1f37113dd3ac56ba42d99db902a4bd69f89185
mezeia77 <mezeia77@gmail.com>
2020-11-23 22:20:21 +0100

    Patient

---
+++ src/main/java/introexception/Patient.java
@@ -0,0 +1,43 @@
+package introexception;
+
+public class Patient {
+
+    final int MIN_YEAR=1900;
+
+    private String name;
+    private String socialSecurityNumber;
+    private int yearOfBirth;
+
+    public Patient (String name, String socialSecurityNumber, int yearOfBirth){
+        if (name.isEmpty()){
+            throw new IllegalArgumentException("Name is empty!");
+        }
+
+        if (yearOfBirth<=MIN_YEAR){
+            throw new IllegalArgumentException(yearOfBirth + " is too old!");
+        }
+
+
+        if (socialSecurityNumber.length() != 9) {
+                throw new IllegalArgumentException(socialSecurityNumber + " contains 9 numbers!");
+        }
+
+
+
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+        this.socialSecurityNumber = socialSecurityNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSocialSecurityNumber() {
+        return socialSecurityNumber;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+}
