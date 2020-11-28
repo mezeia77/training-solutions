1d0b2c554b43a71a1aefc761ba38212ec5986680
mezeia77 <mezeia77@gmail.com>
2020-11-28 19:44:01 +0100

    DuplicatesTest

---
+++ src/test/java/controladvanced/duplicates/DuplicatesTest.java
@@ -0,0 +1,18 @@
+package controladvanced.duplicates;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DuplicatesTest {
+
+    @Test
+
+    void find(){
+        assertEquals(Arrays.asList(5,1), (new Duplicates().find(Arrays.asList(1,2,3,4,5,6,7,8,9, 5,1))));
+
+    }
+
+}
