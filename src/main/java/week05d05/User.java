52485d7b4047f204f2a26905c3b32a911fa92425
mezeia77 <mezeia77@gmail.com>
2020-11-29 19:22:54 +0100

    User

--- src/main/java/week05d05/User.java
+++ src/main/java/week05d05/User.java
@@ -2,15 +2,6 @@ package week05d05;
 
 public class User {
 
-    private String firstName;
-    private String lastName;
-    private String email;
-
-    /*public User (String firstName, String lastName, String email){
-        this.firstName=firstName;
-        this.lastName=lastName;
-        this.email=email;
-    }*/
 
     public String fullName(String firstName, String lastName) {
         return firstName.concat(" ").concat(lastName);
