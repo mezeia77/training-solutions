050c4ea069c39a2cb48cbf98417976f5fb3d3af0
mezeia77 <mezeia77@gmail.com>
2020-12-03 18:52:37 +0100

    Stringconcat

---
+++ src/main/java/stringconcat/Employee.java
@@ -0,0 +1,39 @@
+package stringconcat;
+
+public class Employee {
+
+    private String name;
+    private String job;
+    private int salary;
+
+
+    public Employee(String name, String job, int salary) {
+        this.name = name;
+        this.job = job;
+        this.salary = salary;
+
+        if (name.isBlank() || job == null || job.isBlank() || salary<1){
+            throw new IllegalArgumentException("Can't be zero or empty");
+        }
+
+        if (salary%1000!=0){
+            throw new IllegalArgumentException("Salary must be divided by 1000");
+        }
+    }
+
+    public String toString() {
+        return name + " - " + job + " - " + salary + " Ft";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+}
---
+++ src/test/java/stringconcat/EmployeeTest.java
@@ -0,0 +1,43 @@
+package stringconcat;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EmployeeTest {
+
+    @Test
+    public void testCreate() {
+        Employee employee = new Employee("Kis Géza", "minőségellenőr", 520000);
+
+        assertEquals("Kis Géza", employee.getName());
+        assertEquals("minőségellenőr", employee.getJob());
+        assertEquals(520000, employee.getSalary());
+    }
+
+    @Test
+    public void testCreateWithEmptyName() {
+
+        System.out.println(new Employee("", "minőségellenőr", 520000));
+    }
+
+    @Test
+    public void testCreateWithNullJob() {
+
+        System.out.println(new Employee("Kis Géza", null, 520000));
+    }
+
+    @Test
+    public void testCreateWithNegativeSalary() {
+
+        System.out.println(new Employee("Kis Géza", "minőségellenőr", -520000));
+    }
+
+    @Test
+    public void testToString() {
+        Employee employee = new Employee("Kis Géza", "minőségellenőr", 1000);
+
+        assertEquals("Kis Géza - minőségellenőr - 1000 Ft", employee.toString());
+    }
+}
