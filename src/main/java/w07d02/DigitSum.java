0113711a0aee01b245e8b25a966d128217fd8122
mezeia77 <mezeia77@gmail.com>
2020-12-08 21:39:09 +0100

    W07d02

---
+++ src/main/java/w07d02/DigitSum.java
@@ -0,0 +1,15 @@
+package w07d02;
+
+public class DigitSum {
+
+    public int sumOfDigits(int x){
+        int result=0;
+        x=Math.abs(x);
+
+        while (x>=1){
+            result = result+x%10;
+            x=x/10;
+
+        } return result;
+    }
+}
---
+++ src/test/java/w07d02/DigitSumTest.java
@@ -0,0 +1,14 @@
+package w07d02;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DigitSumTest {
+    DigitSum d = new DigitSum();
+
+    @Test
+    public void sumOfDigitsTest(){
+        assertEquals(10, d.sumOfDigits(-1234));
+    }
+}
