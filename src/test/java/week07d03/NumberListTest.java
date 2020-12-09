ea04dfccc1673e478b7d34960c9d6c40073d8a03
mezeia77 <mezeia77@gmail.com>
2020-12-09 20:20:48 +0100

    week07d03

--- src/test/java/week07d03/NumberListTest.java
+++ src/test/java/week07d03/NumberListTest.java
@@ -4,13 +4,15 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class NumberListTest {
 
     @Test
 
     void isNumberListTest(){
+        assertTrue(new NumberList().isIncreasing(Arrays.asList(1,2,3,4)));
+        assertFalse(new NumberList().isIncreasing(Arrays.asList(1,2,3,4,3)));
         assertEquals(true, new NumberList().isIncreasing(Arrays.asList(1,2,3)));
         assertEquals(false, new NumberList().isIncreasing(Arrays.asList(1,2,3,2)));
 
