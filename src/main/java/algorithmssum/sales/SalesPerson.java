dde4f7a112df9020c8121ee826701502542be647
mezeia77 <mezeia77@gmail.com>
2020-12-12 16:44:00 +0100

    Recursion

---
+++ src/main/java/algorithmssum/sales/SalesPerson.java
@@ -0,0 +1,19 @@
+package algorithmssum.sales;
+
+public class SalesPerson {
+    private String name;
+    private int amount;
+
+    public SalesPerson(String name, int amount) {
+        this.name = name;
+        this.amount = amount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+}
---
+++ src/test/java/recursion/FactorialCalculatorTest.java
@@ -0,0 +1,14 @@
+package recursion;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FactorialCalculatorTest {
+
+    @Test
+    public void isFactorialCorrect(){
+
+        assertEquals(new FactorialCalculator().getFactorial(4), 24);
+    }
+}
