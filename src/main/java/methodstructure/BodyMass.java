8f58d647d59621aa75a417fffd1c19c0096a536e
mezeia77 <mezeia77@gmail.com>
2020-12-13 17:38:03 +0100

    methodstructure

---
+++ src/main/java/methodstructure/BodyMass.java
@@ -0,0 +1,37 @@
+package methodstructure;
+
+public class BodyMass {
+
+    private int weight;
+    private int height;
+
+    public BodyMass(int weight, int height) {
+        this.weight = weight;
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public double bodyMassIndex(){
+        return weight*10000/(height*height);
+    }
+
+    public BmiCategory body(){
+        double bmi = bodyMassIndex();
+        if (bmi < 18.5)
+            return BmiCategory.UNDERWEIGHT;
+        if (bmi > 25)
+            return BmiCategory.OVERWEIGHT;
+        return BmiCategory.NORMAL;
+    }
+
+    public boolean isThinnerThan (BodyMass other) {
+        return bodyMassIndex() < other.bodyMassIndex();
+    }
+}
