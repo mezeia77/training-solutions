ec035a4aea18307575d073cc9194928a2853cb62
mezeia77 <mezeia77@gmail.com>
2020-12-18 18:18:21 +0100

    classstructureattributes

--- src/main/java/classstructureattributes/client.java
+++ src/main/java/classstructureattributes/client.java
@@ -1,12 +1,12 @@
 package classstructureattributes;
 
 public class client {
-    public static void main(String[] args) {
+
 
         String name;
         int yearOfBirth;
         String address;
 
 
-    }
+
 }
