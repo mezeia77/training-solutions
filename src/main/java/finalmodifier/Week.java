b76260a47360e41b6a57ebff9bcba806758cbe89
mezeia77 <mezeia77@gmail.com>
2020-12-19 17:57:45 +0100

    finalmodifier

--- src/main/java/finalmodifier/Week.java
+++ src/main/java/finalmodifier/Week.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class Week {
 
-    public static List<String> DAYS = Arrays.asList("MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY", "SUNDAY");
+    public static final List<String> DAYS = Arrays.asList("MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY", "SUNDAY");
 
     public static void main(String[] args) {
         DAYS.set(1, "Szerda");
