2c07331a997a2c135f133b0d77fd51a16ef3a37c
mezeia77 <mezeia77@gmail.com>
2020-12-21 20:14:03 +0100

    methodoverloading

---
+++ src/main/java/controlselection/greetings/accents/WithoutAccents.java
@@ -0,0 +1,28 @@
+package controlselection.greetings.accents;
+
+public class WithoutAccents {
+
+    private String accentedChars ="áéíóöőüű";
+
+    public char withoutAccent(char c){
+        if (accentedChars.indexOf(c)>=0){
+            switch (c){
+                case 'á':
+                    return 'a';
+                case 'é':
+                    return 'e';
+                case 'í':
+                    return 'i';
+                case 'ó':
+                case 'ö':
+                case 'ő':
+                    return 'o';
+                case 'ú':
+                case 'ü':
+                case 'ű':
+                    return 'u';
+            }
+        } return c;
+    }
+
+}
---
+++ src/test/java/controlselection/greetings/accents/WithoutAccentsTest.java
@@ -0,0 +1,28 @@
+package controlselection.greetings.accents;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class WithoutAccentsTest {
+    WithoutAccents withoutAccents = new WithoutAccents();
+    @Test
+    void woAccentsTest(){
+        assertEquals('a', withoutAccents.withoutAccent('a'));
+        assertEquals('a', withoutAccents.withoutAccent('á'));
+        assertEquals('e', withoutAccents.withoutAccent('e'));
+        assertEquals('e', withoutAccents.withoutAccent('é'));
+        assertEquals('i', withoutAccents.withoutAccent('i'));
+        assertEquals('i', withoutAccents.withoutAccent('í'));
+        assertEquals('o', withoutAccents.withoutAccent('o'));
+        assertEquals('o', withoutAccents.withoutAccent('ó'));
+        assertEquals('o', withoutAccents.withoutAccent('ö'));
+        assertEquals('o', withoutAccents.withoutAccent('ő'));
+        assertEquals('u', withoutAccents.withoutAccent('u'));
+        //assertEquals('u', withoutAccents.withoutAccent('ú'));
+        assertEquals('u', withoutAccents.withoutAccent('ü'));
+        assertEquals('u', withoutAccents.withoutAccent('ű'));
+
+        assertEquals('d', withoutAccents.withoutAccent('d'));
+    }
+}
