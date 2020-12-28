d7d660aebcbd104e1f4b03d238952a5ece41eb02
mezeia77 <mezeia77@gmail.com>
2020-12-28 17:12:09 +0100

    stringmethods

--- src/main/java/stringmethods/url/UrlManager.java
+++ src/main/java/stringmethods/url/UrlManager.java
@@ -1,7 +1,129 @@
 package stringmethods.url;
 
-import java.util.Locale;
-
 public class UrlManager {
 
+    private String protocol;
+    private Integer port;
+    private String host;
+    private String path;
+    private String query;
+
+    public String getProtocol() {
+        return protocol;
+    }
+
+    public Integer getPort() {
+        return port;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public UrlManager(String url) {
+        this.protocol = getProtocolFromUrl(url);
+        this.port = getPortFromUrl(url);
+        this.host = getHostFromUrl(url);
+        this.path = getPathFromUrl(url);
+        this.query = getQueryFromUrl(url);
+    }
+
+    public boolean hasProperty(String key) {
+        validateString(key);
+        return query.startsWith(key + "=") || query.contains("&" + key + "=");
+    }
+
+    public String getProperty(String key) {
+        validateString(key);
+        String[] properties = query.split("&");
+        for (String str : properties) {
+            String[] prop = str.split("=");
+            if (prop[0].equals(key)) {
+                return prop[1];
+            }
+        }
+        return null;
+    }
+
+    private static String getProtocolFromUrl(String url) {
+        int colonPosition = url.indexOf(":");
+        if (!validateUrl(url)) {
+            throw new IllegalArgumentException("Invalid URL!");
+        }
+        return url.toLowerCase().substring(0, colonPosition);
+    }
+
+    private static String getHostFromUrl(String url) {
+        int thirdSlashPosition = url.indexOf("/", 8);
+        int protocolHostBorderPosition = url.indexOf("://");
+        int secondColonPosition = url.indexOf(":", 8);
+        if (!validateUrl(url)) {
+            throw new IllegalArgumentException("Invalid URL!");
+        }
+        if (secondColonPosition == -1){
+            return url.toLowerCase().substring(protocolHostBorderPosition+3, thirdSlashPosition);
+        }
+        return url.toLowerCase().substring(protocolHostBorderPosition+3, secondColonPosition);
+    }
+
+    private static Integer getPortFromUrl(String url) {
+        int secondColonPosition = url.indexOf(":", 8);
+        int thirdSlashPosition = url.indexOf("/", 8);
+        if (secondColonPosition == -1){
+            return null;
+        }
+        if (thirdSlashPosition != -1){
+            return Integer.valueOf(url.substring(secondColonPosition+1, thirdSlashPosition));
+        }
+        return Integer.valueOf(url.substring(secondColonPosition+1));
+    }
+
+    private static String getPathFromUrl(String url) {
+        int thirdSlashPosition = url.indexOf("/", 8);
+        int questionMarkPosition = url.indexOf("?");
+        if (thirdSlashPosition == -1 || questionMarkPosition == -1){
+            return "";
+        }
+
+        return url.substring(thirdSlashPosition, questionMarkPosition);
+    }
+
+    private static String getQueryFromUrl(String url) {
+        int questionMarkPosition = url.indexOf("?");
+        if (questionMarkPosition == -1){
+            return "";
+        }
+        return url.substring(questionMarkPosition+1);
+    }
+
+    private void validateString(String str) {
+        if (str == null || str.isBlank()) {
+            throw new IllegalArgumentException("Parameter must not be empty");
+        }
+    }
+
+    private static boolean validateUrl(String url) {
+        int protocolHostBorderPosition = url.indexOf("://");
+        if (protocolHostBorderPosition == -1){
+            return false;
+        }
+        return true;
+    }
+
+
+public static void main(String[] args) {
+        String url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02";
+
+    System.out.println("Protocol: " + getProtocolFromUrl(url));
+    System.out.println("Host: " + getHostFromUrl(url));
+    System.out.println("Path: " + getPathFromUrl(url));
+    System.out.println("Query: " + getQueryFromUrl(url));
+    System.out.println("Port: " + getPortFromUrl(url));
+    }
+
 }
+
--- src/test/java/stringmethods/url/UrlManagerTest.java
+++ src/test/java/stringmethods/url/UrlManagerTest.java
@@ -2,14 +2,137 @@ package stringmethods.url;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Locale;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class UrlManagerTest {
-    String name = "John Doe".toUpperCase();
+class UrlManagerTest {
 
     @Test
-    void test(){
-        System.out.println(name);
+    void testCreate() {
+
+        String url = "HTTPS://EarthQuake.USgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02";
+
+        UrlManager urlManager = new UrlManager(url);
+
+        assertEquals("https", urlManager.getProtocol());
+        assertEquals("earthquake.usgs.gov", urlManager.getHost());
+        assertEquals("/fdsnws/event/1/query", urlManager.getPath());
+        assertNull(urlManager.getPort());
     }
 
+    @Test
+    void testCreateWithPortAtEnd() {
+
+        String url = "HTTPS://EarthQuake.USgs.gov:80";
+
+        UrlManager urlManager = new UrlManager(url);
+
+        assertEquals("https", urlManager.getProtocol());
+        assertEquals("earthquake.usgs.gov", urlManager.getHost());
+        assertEquals("", urlManager.getPath());
+        assertEquals(Integer.valueOf(80), urlManager.getPort());
+    }
+
+    @Test
+    void testCreateWithPort() {
+
+        String url = "HTTPS://EarthQuake.USgs.gov:80/query?a=4";
+
+        UrlManager urlManager = new UrlManager(url);
+
+        assertEquals("https", urlManager.getProtocol());
+        assertEquals("earthquake.usgs.gov", urlManager.getHost());
+        assertEquals("/query", urlManager.getPath());
+        assertEquals(Integer.valueOf(80), urlManager.getPort());
+    }
+
+//    @Test
+//    void testCreateMissingHost() {
+//        exception.expect(IllegalArgumentException.class);
+//        exception.expectMessage("Invalid url");
+//
+//        new UrlManager("http://");
+//    }
+
+//    @Test
+//    void testCreateMissingProtocol() {
+//        exception.expect(IllegalArgumentException.class);
+//        exception.expectMessage("Invalid url");
+//
+//        new UrlManager("www.google.com");
+//    }
+
+    @Test
+    void testHasProperty() {
+        String url = "HTTPS://EarthQuake.USgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02";
+
+        UrlManager urlManager = new UrlManager(url);
+
+        assertTrue(urlManager.hasProperty("format"));
+    }
+
+    @Test
+    void testHasPropertyMissing() {
+        String url = "HTTPS://EarthQuake.USgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02";
+
+        UrlManager urlManager = new UrlManager(url);
+
+        assertFalse(urlManager.hasProperty("place"));
+    }
+
+    @Test
+    void testHasPropertyForKeypart() {
+        String url = "HTTPS://EarthQuake.USgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02";
+
+        UrlManager urlManager = new UrlManager(url);
+
+        assertFalse(urlManager.hasProperty("time"));
+    }
+
+//    @Test
+//    void testHasPropertyWithNull() {
+//        String url = "HTTPS://EarthQuake.USgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02";
+//        UrlManager urlManager = new UrlManager(url);
+//
+//        exception.expect(IllegalArgumentException.class);
+//
+//        urlManager.hasProperty(null);
+//    }
+
+    @Test
+    void testGetProperty() {
+        String url = "HTTPS://EarthQuake.USgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02";
+
+        UrlManager urlManager = new UrlManager(url);
+
+        assertEquals("geojson", urlManager.getProperty("format"));
+    }
+
+    @Test
+    void testGetPropertyMissing() {
+        String url = "HTTPS://EarthQuake.USgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02";
+
+        UrlManager urlManager = new UrlManager(url);
+
+        assertNull(urlManager.getProperty("place"));
+    }
+
+    @Test
+    void testGetPropertyForKeyPart() {
+        String url = "HTTPS://EarthQuake.USgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02";
+
+        UrlManager urlManager = new UrlManager(url);
+
+        assertNull(urlManager.getProperty("time"));
+    }
+
+
+//    @Test
+//    void testGetPropertyWithBlankParameter() {
+//        String url = "HTTPS://EarthQuake.USgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02";
+//        UrlManager urlManager = new UrlManager(url);
+//
+//        exception.expect(IllegalArgumentException.class);
+//
+//        urlManager.hasProperty("  ");
+//    }
 }
