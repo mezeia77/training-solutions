561b2c85957d93902eda3d49fb34b431a4b5b794
mezeia77 <mezeia77@gmail.com>
2020-12-30 12:55:09 +0100

    stringbuilder

--- src/test/java/stringbuilder/PalindromeTest.java
+++ src/test/java/stringbuilder/PalindromeTest.java
@@ -6,14 +6,31 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PalindromeTest {
 
-    @Test
+    /*@Rule
+    public final ExpectedException exception = ExpectedException.none();
 
+    @Test
+    public void nullParameterShouldThrowException() throws IllegalArgumentException {
+
+        Exception exception = assertThrows(IllegalArgumentException.class, ()-> new Palindrome("", , ));
+        assertThrows("va", IllegalArgumentException);
+
+        exception.expect(IllegalArgumentException.class);
+        exception.expectMessage("Text must not be null!");
+
+        // When
+        new PalindromeValidator().isPalindrome(null);
+    }*/
+
+    @Test
     public void validPalindrome() {
 
         assertEquals(new Palindrome().isPalindrome("Racecar"), true);
         assertEquals(new Palindrome().isPalindrome("start"), false);
         assertEquals(new Palindrome().isPalindrome("Apa"), true);
         assertEquals(new Palindrome().isPalindrome("11"), true);
+
+
     }
 
 }
