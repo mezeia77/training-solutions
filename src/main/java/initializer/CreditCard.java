dbfef52106294a0539e8750e77b64b3a27d99d68
mezeia77 <mezeia77@gmail.com>
2021-01-01 14:14:31 +0100

    initializer

---
+++ src/main/java/initializer/CreditCard.java
@@ -0,0 +1,44 @@
+package initializer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CreditCard {
+
+    private static final List<Rate> ACTUAL_RATES = new ArrayList<>();
+    private long balance;
+
+    public CreditCard(long balance, Currency currency, List<Rate> upload) {
+        this.balance = Math.round(balance * getConverionRate(currency));
+        ACTUAL_RATES.addAll(upload);
+    }
+
+    public CreditCard(long balance) {
+        this.balance = balance;
+    }
+
+    public long getBalance() {
+        return balance;
+    }
+
+    public boolean payment(long amount, Currency currency){
+        long value = Math.round(amount*getConverionRate(currency));
+        if(value<=balance){
+            balance = balance-value;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean payment (long amount){
+        return payment(amount, Currency.HUF);
+    }
+
+    private double getConverionRate(Currency currency){
+        for (Rate rate : ACTUAL_RATES){
+            if(rate.getCurrency() == currency){
+                return rate.getConversionfactor();
+            }
+        } return 1;
+    }
+}
---
+++ src/main/java/initializer/Currency.java
@@ -0,0 +1,5 @@
+package initializer;
+
+public enum Currency {
+    HUF, EUR, USD, GBP, SFR
+}
