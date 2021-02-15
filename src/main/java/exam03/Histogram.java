32739f61254163f32e1f0152ffa1a598d99d9a5f
mezeia77 <mezeia77@gmail.com>
2021-02-15 18:52:49 +0100

    exam03

---
+++ src/main/java/exam03/Histogram.java
@@ -0,0 +1,24 @@
+package exam03;
+
+public class Histogram {
+}
+
+//    Olvass be egy szöveges fájlt soronként, melyben soronkont számok vannak.
+//        Majd hozz létre egy stringet, mely ugyanennyi sort tartalmaz,
+//        és annyi csillagot ír ki egymás mellé soronkont, amennyi a bemeneti fájlban lévő szám.
+//
+//        Azaz a bemeneti fájl:
+//
+//        3
+//        4
+//        1
+//        2
+//        A létrehozott String:
+//
+//        ***
+//        ****
+//        *
+//        **
+//        Figyelj, hogy az algoritmusod lehetőleg hatékony legyen!
+//
+//        A Histogram osztályba dolgozz, tesztje HistogramTest. Minden sor után legyen sortörés, az utolsó után is.
