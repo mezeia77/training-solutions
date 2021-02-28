f7b4c10a0e298256a62a2fc2c0f8059858393425
mezeia77 <mezeia77@gmail.com>
2021-02-28 12:16:44 +0100

    jdbc

--- src/main/java/jdbc/EmployeesMain.java
+++ src/main/java/jdbc/EmployeesMain.java
@@ -16,13 +16,14 @@ public class EmployeesMain {
         dataSource.setPassword("employees");
     }
 
-    public void insert(){
-        try {
-            prepare();
-            Connection conn = dataSource.getConnection();
+    public void insert() throws SQLException {
+        prepare();
+        try (Connection conn = dataSource.getConnection();
 //            Statement stmt = conn.createStatement();
 //            stmt.executeUpdate("insert into employees(emp_name) values ('John Doe')");
-            PreparedStatement stmt = conn.prepareStatement("insert into employees(emp_name) values (?)");
+             PreparedStatement stmt = conn.prepareStatement("insert into employees(emp_name) values (?)")
+        ){
+
             stmt.setString(1, "Jack Doe");
             stmt.executeUpdate();
         }
@@ -36,12 +37,13 @@ public class EmployeesMain {
         try (
             Connection conn = dataSource.getConnection();
             Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery("select emp_name from employees");
+            ResultSet rs = stmt.executeQuery("select id,emp_name from employees");
         ){
             List<String> names = new ArrayList<>();
             while ((rs.next())){
              String name = rs.getString("emp_name");
              names.add(name);
+                System.out.println(rs.getLong("id"));
             }
             System.out.println(names);
         } catch (SQLException se) {
@@ -77,7 +79,7 @@ public class EmployeesMain {
 
     public static void main(String[] args) throws SQLException {
 //        new EmployeesMain().insert();
-//        new EmployeesMain().select();
-        new EmployeesMain().findById();
+        new EmployeesMain().select();
+//        new EmployeesMain().findById();
     }
 }
