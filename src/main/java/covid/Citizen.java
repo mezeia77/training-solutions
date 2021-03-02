a0e3c1b5f8b891922f155c48c8b338ae498d47bb
mezeia77 <mezeia77@gmail.com>
2021-03-02 12:39:34 +0100

    covid

---
+++ src/main/java/covid/Citizen.java
@@ -0,0 +1,16 @@
+package covid;
+
+import java.time.LocalDate;
+
+public class Citizen {
+
+    private String fullName;
+    private String ZIP;
+    private int age;
+    private String email;
+    private String TAJ;
+    private int numberOfVaccination;
+    private LocalDate lastVaccination;
+
+
+}
