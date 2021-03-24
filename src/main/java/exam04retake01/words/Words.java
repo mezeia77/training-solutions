c794f0189f9e1da4f658256e470de289ff33eb67
mezeia77 <mezeia77@gmail.com>
2021-03-24 21:37:50 +0100

    exam04retake01

--- src/main/java/exam04retake01/words/Words.java
+++ src/main/java/exam04retake01/words/Words.java
@@ -1,4 +1,13 @@
 package exam04retake01.words;
 
 public class Words {
+    public boolean hasMoreDigits(String s) {
+        int counter = 0;
+        for (int i = 0; i<s.length(); i++){
+            if(Character.isDigit(s.charAt(i))){
+                counter++;
+            }
+        }
+        return counter>s.length()/2;
+    }
 }
